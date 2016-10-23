2167bb95e6b9354dbffd9a8e5ff142ab37972dbf
Tarik EN NAKDI <t.ennakdi@gmail.com>
2016-10-23 12:09:02 +0200

    Create script.jsx

---
+++ TotalCash/script.jsx
@@ -0,0 +1,44 @@
+var Product = React.createClass({
+	getInitialState: function(){
+		return {qty: 0};
+  },
+	buy: function(){
+		this.setState({qty: this.state.qty + 1});
+  },
+  
+  render: function(){
+    return(
+		<div>
+			<p>Android - 199$</p>
+			<button onClick={this.buy}>Buy</button>
+			<h3>Qty: {this.state.qty} item(s)</h3>
+		</div>
+      );
+  }
+});
+
+var Total = React.createClass({
+	render: function(){
+		return(
+			<div>
+				<h3>Total Cash:</h3>
+			</div>
+		);
+	}
+});
+
+var ProductList = React.createClass({
+	render: function(){
+	return(
+		<div>
+			<Product/>
+			<Product/>
+			<Product/>
+			<Total/>
+		</div>
+	);
+	}
+
+});
+
+React.render( <ProductList/>, document.getElementById("root"));
