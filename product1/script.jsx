21959b63ec306a45bf6e45f6f602412d9ecb7b9e
Tarik EN NAKDI <t.ennakdi@gmail.com>
2016-10-23 12:00:28 +0200

    Create script.jsx

---
+++ product1/script.jsx
@@ -0,0 +1,20 @@
+var Product = React.createClass({
+	getInitialState: function(){
+		return {qty: 0};
+  },
+	buy: function(){
+		this.setState({qty: this.state.qty + 1});
+  },
+  
+  render: function(){
+    return(
+		<div>
+			<p>Android - 199$</p>
+			<button onClick={this.buy}>Buy</button>
+			<h3>Qty: {this.state.qty} item(s)</h3>
+		</div>
+      );
+  }
+});
+
+React.render( <Product/>, document.getElementById("root"));
